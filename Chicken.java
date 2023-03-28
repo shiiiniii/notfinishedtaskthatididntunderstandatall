38be0ea734bec5c43d7196d1b28f382b0ce162d8
shiiiniii <115028625+shiiiniii@users.noreply.github.com>
2023-03-28 06:47:58 +0300

    Create Chicken.java

---
+++ Chicken.java
@@ -0,0 +1,16 @@
+package finalwork.Animals;
+import finalwork.*;
+public class Chicken extends Bird{
+    public String getName() {
+        return "курица";
+    }
+    @Override
+    public void Fly() {
+        System.out.printf("курица не летает", this.Chicken();
+    }    
+	
+
+    
+    
+    
+}
